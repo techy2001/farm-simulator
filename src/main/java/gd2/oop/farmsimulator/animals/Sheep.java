bb1b2f9bafe45193cee3269c8f98678bf035d61c
Techy2001 <74059089+techy2001@users.noreply.github.com>
2022-11-27 23:55:50 +0000

    fix: remove example code

--- src/main/java/gd2/oop/farmsimulator/animals/Sheep.java
+++ src/main/java/gd2/oop/farmsimulator/animals/Sheep.java
@@ -3,11 +3,4 @@ package gd2.oop.farmsimulator.animals;
 import gd2.oop.farmsimulator.animals.raw.Animal;
 import gd2.oop.farmsimulator.interfaces.IMilkable;
 
-public class Sheep extends Animal {
-
-    @Override
-    public String getName() {
-        return "Sheepname";
-    }
-}
-
+public class Sheep extends Animal {}
